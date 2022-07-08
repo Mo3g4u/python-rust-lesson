b7fdb7847e43b3117b2bcd3f5013f02513f1f17e
Mo3g4u <lax34volvic@gmail.com>
2022-07-08 18:01:28 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 書籍『高効率言語 Rust 書き方・作り方』学習
+
+- [サンプルコード](https://github.com/kujirahand/book-rust)
---
+++ ch1/show_price.rs
@@ -0,0 +1,5 @@
+// Rustで数値を文字列に埋め込んで表示
+fn main() {
+    let banana = 300; // 値を変数に束縛
+    println!("バナナの値段={}円", banana)
+}
